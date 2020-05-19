88e104386c9d38c43dcb7f3adf7ff3f725d0e268
Carl Eiserman <carleiserman@gmail.com>
2020-05-19 22:06:40 +0200

    chore: added readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Imperative vs Functional solutions - A JSPerf Journey
+
+- [Tests on JSPerf](https://jsperf.com/imperative-index-vs-functional-index)
+- TODO: Create this readme
--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const nestedTreeMock = [
 
 
 const groupedActions = {
-  "1": new Array(7).map(item => { }),
+  "1": new Array(7).map(item => {}),
   "1.1.1": [{}, {}, {}],
   "2.1": [{}, {}, {}],
   "2.2": [{}, {}]
@@ -42,47 +42,13 @@ const getTaskAmountForId = (searchId, actionGroup) => {
   return actionGroup[searchId].length;
 }
 
-const getGlobalIndicesImperative = (treeArr, actionGroup) => {
-  const globalIndices = {};
-
-  const addToIndices = (id, total) => {
-    globalIndices[id] = total;
-  }
-
-  const getCountsForAllChildren = (entry) => {
-    const { children, id: rootId } = entry;
-
-    const allChildrenAmount = children.reduce((total, currentChild) => {
-      const totalForCurrentEntry = getTaskAmountForId(currentChild.id, actionGroup);
-
-      if (!currentChild.children || !currentChild.children.length) {
-        addToIndices(currentChild.id, totalForCurrentEntry);
-        return total + totalForCurrentEntry;
-      }
-
-      const childCounts = getCountsForAllChildren(currentChild);
-      const currentPlusChildren = childCounts + totalForCurrentEntry;
-      addToIndices(currentChild.id, currentPlusChildren);
-      return total + currentPlusChildren;
-    }, 0);
-
-    const rootTotal = allChildrenAmount + getTaskAmountForId(rootId, actionGroup);
-    addToIndices(rootId, rootTotal);
-
-    return rootTotal;
-  }
-
-  treeArr.forEach(entry => getCountsForAllChildren(entry));
-  return globalIndices;
-}
-
-const getIndicesFunctional = (treeArr, actionGroup) => {
+const getIndicesFunctional = (nodes, tasks) => {
   const getCountsForAllChildren = (entry) => {
     const { children, id: rootId } = entry;
 
     const allChildIndices = children.reduce((accumulator, currentChild) => {
       if (!currentChild.children || !currentChild.children.length) {
-        const totalForCurrentEntry = getTaskAmountForId(currentChild.id, actionGroup);
+        const totalForCurrentEntry = getTaskAmountForId(currentChild.id, tasks);
         return {
           total: accumulator.total + totalForCurrentEntry,
           indices: {
@@ -96,7 +62,7 @@ const getIndicesFunctional = (treeArr, actionGroup) => {
         total: accumulator.total + childCounts.total,
         indices: {
           ...childCounts.indices,
-          [currentChild.id]: childCounts.total + getTaskAmountForId(currentChild.id, actionGroup), // if tasks at all levels
+          [currentChild.id]: childCounts.total + getTaskAmountForId(currentChild.id, tasks), // if tasks at all levels
         }
       };
     }, {
@@ -108,12 +74,12 @@ const getIndicesFunctional = (treeArr, actionGroup) => {
       total: allChildIndices.total,
       indices: {
         ...allChildIndices.indices,
-        [rootId]: allChildIndices.total + getTaskAmountForId(rootId, actionGroup),
+        [rootId]: allChildIndices.total + getTaskAmountForId(rootId, tasks),
       }
     }
   }
 
-  const result = treeArr.reduce((acc, currentEntry) => {
+  const result = nodes.reduce((acc, currentEntry) => {
     const { indices, total } = getCountsForAllChildren(currentEntry);
     return {
       ...indices,
@@ -123,6 +89,212 @@ const getIndicesFunctional = (treeArr, actionGroup) => {
   return result;
 }
 
-const results = getGlobalIndicesImperative(nestedTreeMock, groupedActions);
+const getGlobalIndicesImperative = (nodes, tasks) => {
+  const globalIndices = {};
 
-console.log('all output: ', JSON.stringify(results, null, 2));
+  const addToIndices = (id, total) => {
+    globalIndices[id] = total;
+  }
+
+  const getCountsForAllChildren = (entry) => {
+    const { children, id: rootId } = entry;
+
+    const allChildrenAmount = children.reduce((total, currentChild) => {
+      const totalForCurrentEntry = getTaskAmountForId(currentChild.id, tasks);
+
+      if (!currentChild.children || !currentChild.children.length) {
+        addToIndices(currentChild.id, totalForCurrentEntry);
+        return total + totalForCurrentEntry;
+      }
+
+      const childCounts = getCountsForAllChildren(currentChild);
+      const currentPlusChildren = childCounts + totalForCurrentEntry;
+      addToIndices(currentChild.id, currentPlusChildren);
+      return total + currentPlusChildren;
+    }, 0);
+
+    const rootTotal = allChildrenAmount + getTaskAmountForId(rootId, tasks);
+    addToIndices(rootId, rootTotal);
+
+    return rootTotal;
+  }
+
+  nodes.forEach(entry => getCountsForAllChildren(entry));
+  return globalIndices;
+}
+
+/**
+ * It was at this point we wanted to try Mw's own solution.
+ * - Copy pasted from slack
+ * - No errors
+ * - Pasted straight into JSPerf
+ * - Cryptic errors, line numbers mentioned
+ * - Nothing seems wrong in editor
+ * - Install and configure ESLint locally
+ * - Spotted a ghost (newline?) character that wasn't compatible and invisibly breaking the code
+ * - Code works
+ * - WTF code seemingly looked way faster
+ * - Code was slower
+ * - Tried fixing immediate bugs
+ * - Made the code even worse???
+ * - Using a closure somehow speeds it up
+ * - Adding conditionals made it slightly faster
+ * 
+ * ... 
+ * 
+ * At this point we decided to remove the console.log and JSON.stringify from the JSPerf test cases
+ * wow... turns out this was choking all of the tests. 
+ * 
+ * Results 
+ * 
+ */
+
+/** OOPS */
+
+const getChildTasksCount = (node, tasks) => {
+    let currentCount;
+
+    if (tasks[node.id] && tasks[node.id].length) {
+      currentCount = tasks[node.id].length;
+    } else {
+      currentCount = 0;
+    }
+  
+    const childCounts = node.children.reduce((childCounts, child) => {
+      const count = getChildTasksCount(child, tasks);
+      currentCount += count[child.id];
+  
+      return {
+        ...childCounts,
+        ...count,
+      }
+    }, {})
+  
+    return {
+      ...childCounts,
+      [node.id]: currentCount,
+    };
+}
+
+const getGlobalIndicesMw = (nodes, tasks) => {
+  return nodes.reduce((counts, node) => {
+    const nodeCounts = getChildTasksCount(node, tasks);
+    return {
+      ...counts,
+      ...nodeCounts,
+    };
+  }, {})
+}
+
+/** YAY */
+
+const getChildTasksCount2 = (node, tasks) => {
+    let currentCount;
+
+    if (tasks[node.id] && tasks[node.id].length) {
+      currentCount = tasks[node.id].length;
+    } else {
+      currentCount = 0;
+    }
+  
+    const childCounts = node.children.reduce((acc, child) => {
+      const count = getChildTasksCount(child, tasks);
+      currentCount += count[child.id];
+  
+      return {
+        ...acc,
+        ...count,
+      }
+    }, {})
+  
+    return {
+      ...childCounts,
+      [node.id]: currentCount,
+    };
+}
+
+const getGlobalIndicesMw2 = (nodes, tasks) => {
+  return nodes.reduce((counts, node) => {
+    const nodeCounts = getChildTasksCount2(node, tasks);
+    return {
+      ...counts,
+      ...nodeCounts,
+    };
+  }, {})
+}
+
+/** Attempt 3 - Closures */
+
+const getGlobalIndicesMw3 = (nodes, tasks) => {
+  const getChildTasksCount = (node) => {
+    let currentCount;
+
+    if (tasks[node.id] && tasks[node.id].length) {
+      currentCount = tasks[node.id].length; 1
+    } else {
+      currentCount = 0;
+    }
+
+    const childCounts = node.children.reduce((acc, child) => {
+      const count = getChildTasksCount(child);
+      currentCount += count[child.id];
+
+      return {
+        ...acc,
+        ...count,
+      }
+    }, {})
+
+    return {
+      ...childCounts,
+      [node.id]: currentCount,
+    };
+  }
+
+  return nodes.reduce((counts, node) => {
+    const nodeCounts = getChildTasksCount(node);
+    return {
+      ...counts,
+      ...nodeCounts,
+    };
+  }, {})
+}
+
+/** Attempt 4 - Conditional checks */
+
+const getGlobalIndicesMw4 = (nodes, tasks) => {
+  const getChildTasksCount = (node) => {
+    let currentCount = 0;
+
+    if (tasks[node.id] && tasks[node.id].length) {
+      currentCount = tasks[node.id].length; 1
+    }
+
+    if (!node.children || !node.children.length) {
+      return {}
+    }
+
+    const childCounts = node.children.reduce((acc, child) => {
+      const count = getChildTasksCount(child);
+      currentCount += count[child.id];
+
+      return {
+        ...acc,
+        ...count,
+      }
+    }, {})
+
+    return {
+      ...childCounts,
+      [node.id]: currentCount,
+    };
+  }
+
+  return nodes.reduce((counts, node) => {
+    const nodeCounts = getChildTasksCount(node);
+    return {
+      ...counts,
+      ...nodeCounts,
+    };
+  }, {})
+}
