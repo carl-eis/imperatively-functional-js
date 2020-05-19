aec3c8974c8e6516c0888546ea08ab3a33613e78
Carl Eiserman <carleiserman@gmail.com>
2020-05-19 20:27:43 +0200

    feat: imperative solution

--- index.ts
+++ index.ts
@@ -57,53 +57,47 @@ interface IAggregatedCountIndices {
 }
 
 
-const getCountsForAllChildren = (entry: IListEntry): IAggregatedCountIndices => {
-  const { children, id: rootId } = entry;
 
-  const allChildIndices = children.reduce((accumulator: IAggregatedCountIndices, currentChild: IListEntry) => {
-    if (currentChild.children && currentChild.children.length) {
-      const childCounts = getCountsForAllChildren(currentChild);
-      return {
-        total: accumulator.total + childCounts.total,
-        indices: {
-          ...childCounts.indices,
-          [currentChild.id]: childCounts.total + getTaskAmountForId(currentChild.id), // if tasks at all levels
-        }
-      } as IAggregatedCountIndices;
-    } else {
-      const totalForCurrentEntry: number = getTaskAmountForId(currentChild.id);
-
-      return {
-        total: accumulator.total + totalForCurrentEntry,
-        indices: {
-          ...accumulator.indices,
-          [currentChild.id]: totalForCurrentEntry
-        }
-      } as IAggregatedCountIndices;
-    }
-  }, {
-    total: 0,
-    indices: {},
-  } as IAggregatedCountIndices);
-
-  return {
-    total: allChildIndices.total,
-    indices: {
-      ...allChildIndices.indices,
-      [rootId]: allChildIndices.total + getTaskAmountForId(rootId),
-    }
-  }
-}
 
 const main = () => {
-  const result = nestedTreeMock.reduce((acc, currentEntry) => {
-    const { indices, total } = getCountsForAllChildren(currentEntry);
-    return {
-      ...indices,
-      ...acc,
-    }
-  }, {});
-  console.log('all output: ', JSON.stringify(result, null, 2));
+  const globalIndices = {};
+
+  const addToIndices = (id: string, total: number): void => {
+    globalIndices[id] = total;
+  }
+
+  const getCountsForAllChildren = (entry: IListEntry): number => {
+    const { children, id: rootId } = entry;
+  
+    const allChildrenAmount: number = children.reduce((total: number, currentChild: IListEntry) => {
+      if (currentChild.children && currentChild.children.length) {
+        const childCounts: number = getCountsForAllChildren(currentChild);
+        const currentPlusChildren: number = childCounts + getTaskAmountForId(currentChild.id);
+        addToIndices(currentChild.id, currentPlusChildren);
+        return total + currentPlusChildren;
+      } else {
+        const totalForCurrentEntry: number = getTaskAmountForId(currentChild.id);
+        addToIndices(currentChild.id, totalForCurrentEntry);
+        return total + totalForCurrentEntry;
+      }
+    }, 0);
+  
+    const rootTotal = allChildrenAmount + getTaskAmountForId(rootId);
+    addToIndices(rootId, rootTotal);
+
+    return rootTotal;
+  }
+
+  nestedTreeMock.forEach(entry => getCountsForAllChildren(entry));
+
+  // const result = nestedTreeMock.reduce((acc, currentEntry) => {
+  //   const { indices, total } = getCountsForAllChildren(currentEntry);
+  //   return {
+  //     ...indices,
+  //     ...acc,
+  //   }
+  // }, {});
+  console.log('all output: ', JSON.stringify(globalIndices, null, 2));
 }
 
 main();
